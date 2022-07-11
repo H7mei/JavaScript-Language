49b5571eacdbd95449d62e8d39f5d4506b6ddce3
mecha <hanafichoirulloh1@gmail.com>
2022-07-11 18:12:14 +0700

    init

---
+++ ModERn/2.js
@@ -0,0 +1,78 @@
+// Execution Context, Hoisting and scope
+
+console.log(nama);
+var nama = "hanafichoi";
+
+// creation phasse pada global Context <>1<>
+// yang pertama kali dicari
+// nama var = undifined,
+// nama function =fn ()
+// konsep ini bernama Hoisting
+
+// window = global object
+// this = window
+
+// execution phase <>2<>
+
+var name = "hanafichoi";
+var umur = 33;
+
+console.log(Say());
+
+function Say() {
+  return `Halo, nama saya ${name}, saya ${umur}, tahun.`;
+}
+
+// kalo udah jadi function ia membuat
+// function membuat local execution conext (yang tejadi di belakang layar)
+// yang di dalamanya terdapat creation dan execution phase juga ()
+// bisa akses window atau
+// arguments,
+// hoisting
+
+// scope kalo gada dari yang terdekat ia nyari atasnya
+
+var ig = ["@", "hanafichoi"];
+
+function Whois(ig) {
+  console.log(arguments[0]);
+  var url = "http://instagram.com/";
+  var final = url + ig.pop();
+  return final;
+}
+
+console.log(Whois(ig));
+
+// Tumpukan Eksekusi
+function a() {
+  console.log(`ini a`);
+  function b() {
+    console.log(`ini b`);
+    function c() {
+      console.log(`ini c`);
+    }
+    c();
+  }
+  b();
+}
+a();
+
+// Latihan 1
+function satu() {
+  var nama = "hanafichoi";
+  console.log(nama);
+}
+
+function dua() {
+  console.log(arguments);
+  console.log(nama);
+}
+
+console.log(nama);
+var nama = "joma";
+satu();
+// masuk argument di abaikan
+dua("dood");
+console.log(nama);
+
+// https://pythontutor.com/javascript.html#mode=edit javasriptvisualizer
---
+++ README.md
@@ -0,0 +1,37 @@
+# JavaScript-Language
+
+<div align="center" >
+  <img height="" src="https://i.postimg.cc/JhLLLghj/js.png" />
+</div>
+    
+## isi
+
+- [basic code 🔧](/BAsic/README.md)
+- [DOM 🎓](/DOm/README.md)
+- [Modern 💷](/ModERn/README.md)
+
+## Tentang C
+
+nama : JavaScript<br/>
+sejak : 1995<br/>
+dibuat oleh : [Brendan Eich](https://en.wikipedia.org/wiki/Brendan_Eich)<br/>
+tujuan pembuatan : pada awalnya hanya digunakan di browser web, tetapi sekarang menjadi komponen inti dari beberapa server dan berbagai aplikasi<br/>
+paradigma pemrograman : [multi-paradigma](https://en.wikipedia.org/wiki/Programming_paradigm)<br/>
+situs web resmi : [ecma internasional](https://www.ecma-international.org/publications-and-standards/standards/ecma-262/)<br/>
+learning : [exercism.org](https://exercism.org/tracks/c)
+
+## C compile
+
+```bash
+# run
+node <path>
+
+```
+
+## Berkontribusi
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## Lisensi
+[MIT](/LICENSE)
